881292ac7772377b27b6cb8bd0dd72172ad7ddbb
JoseBC <jbalcazar377@gmail.com>
2025-06-23 16:20:16 -0500

    carpetas listas

---
+++ src/app/features/post/post-inicio/post-inicio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-inicio',
+  imports: [],
+  templateUrl: './post-inicio.html',
+  styleUrl: './post-inicio.scss'
+})
+export class PostInicio {
+
+}
---
+++ src/app/pages/pagina-error/pagina-error.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-error',
+  imports: [],
+  templateUrl: './pagina-error.html',
+  styleUrl: './pagina-error.scss'
+})
+export class PaginaError {
+
+}
