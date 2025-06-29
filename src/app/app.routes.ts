9d89e2566c0c94e2c871b18bc3f2d70f4cfbf37b
JoseBC <jbalcazar377@gmail.com>
2025-06-28 23:33:11 -0500

    movida de formato header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
+import { AdminLayout } from './features/auth/layouts/admin-layout/admin-layout';
+import { ClientLayout } from './features/auth/layouts/client-layout/client-layout';
 
 export const routes: Routes = [
     {
-        path: 'login', component: Login, title: "Iniciar Sesión"
+        path: 'admin', component: AdminLayout, children: [
+
+        ]
     },
-    
+    {
+        path: '', component: ClientLayout, children: [
+            {
+                path: 'login', component: Login, title: "Iniciar Sesión"
+            }
+        ]
+    },
+    // {
+    //     path:'**'
+    // }
+
+
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { Footer } from "./shared/components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer ],
+  imports: [RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
