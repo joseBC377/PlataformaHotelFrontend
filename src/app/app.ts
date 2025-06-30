296ad270d54bfce822f7b51e775ed705fe4d0efa
Daniela <danielasf2002@outlook.es>
2025-06-29 20:53:29 -0500

    estructura para la vista de inicio y estilos

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./shared/components/header/header";
 import { Footer } from "./shared/components/footer/footer";
+import { Inicio } from "./pages/inicio/inicio";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Inicio],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
