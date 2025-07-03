7d2262fe3eb9c32079c73a1e63de59dfadf73bda
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2025-07-03 18:36:24 -0500

    Ruta de Nosotros listo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Nosotros } from './pages/nosotros/nosotros';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'nosotros', component:Nosotros, title:"Nosotros"}
+
+];
