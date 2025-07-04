b65e2a0bd0d1ad897dc2f10f5e5074418367de75
JoseBC <jbalcazar377@gmail.com>
2025-07-03 19:43:48 -0500

    actualización de form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
 import { AdminLayout } from './features/auth/layouts/admin-layout/admin-layout';
 import { ClientLayout } from './features/auth/layouts/client-layout/client-layout';
+import { Intranet } from './features/admin/intranet/intranet';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     {
-        path: 'admin', component: AdminLayout, children: [
-
+        path: 'admin', component: AdminLayout, canActivate:[authGuard], children: [
+            {
+                path:'intranet', component:Intranet, title:'Intranet'
+            }
         ]
     },
     {
--- src/app/features/auth/layouts/admin-layout/admin-layout.ts
+++ src/app/features/auth/layouts/admin-layout/admin-layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from "../../../../shared/components/header/header";
+import { Footer } from "../../../../shared/components/footer/footer";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.scss'
 })
