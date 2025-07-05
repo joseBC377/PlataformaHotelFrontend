fb1504c2ef8f73b80032a1e06a31f27348c20f04
Daniela <danielasf2002@outlook.es>
2025-07-04 23:15:32 -0500

    creacion del nav para admin

---
+++ src/app/shared/components/header-ad/header-ad.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-ad',
+  imports: [],
+  templateUrl: './header-ad.html',
+  styleUrl: './header-ad.scss'
+})
+export class HeaderAd {
+
+}
