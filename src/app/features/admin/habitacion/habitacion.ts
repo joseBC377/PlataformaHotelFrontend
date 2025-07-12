43968675cc49b113c1831dae2339e241c44f0508
Daniela <danielasf2002@outlook.es>
2025-07-11 19:41:11 -0500

    crud listo en habitacion

--- src/app/features/admin/habitacion/habitacion.ts
+++ src/app/features/admin/habitacion/habitacion.ts
@@ -27,7 +27,7 @@ export class HabitacionesAdminComponent implements OnInit {
     nombre: ['', [Validators.required, Validators.minLength(3)]],
     descripcion: ['', Validators.required],
     estado: ['', Validators.required],
-  categoriaHabitacion: [null, Validators.required] 
+    categoriaHabitacion: [null, Validators.required]
   });
 
   get nombre() { return this.habitacionForm.get('nombre'); }
@@ -46,6 +46,8 @@ export class HabitacionesAdminComponent implements OnInit {
     }
 
     const data = this.habitacionForm.value;
+    console.log('Actualizando habitación con ID:', this.idHabitacionEditar);
+    console.log('Datos a enviar:', data);
 
     if (this.modoEdicion) {
       this.serv.putEditarHabitacion(this.idHabitacionEditar!, data).subscribe(() => {
@@ -59,22 +61,20 @@ export class HabitacionesAdminComponent implements OnInit {
       });
     }
   }
-editarHabitacion(hab: Habitacion): void {
-  console.log('Editando habitación:', hab);
+  editarHabitacion(hab: Habitacion): void {
+    console.log('Editando habitación:', hab);
 
-  this.habitacionForm.patchValue({
-    id: hab.id,
-    nombre: hab.nombre,
-    descripcion: hab.descripcion,
-    estado: hab.estado,
-    categoriaHabitacion: hab.categoriaHabitacion 
-  });
+    this.habitacionForm.patchValue({
+      id: hab.id,
+      nombre: hab.nombre,
+      descripcion: hab.descripcion,
+      estado: hab.estado,
+      categoriaHabitacion: hab.categoriaHabitacion
+    });
 
-  this.modoEdicion = true;
-}
-
-
-
+    this.idHabitacionEditar = hab.id!;
+    this.modoEdicion = true;
+  }
 
   eliminarHabitacion(id: number): void {
     if (confirm('¿Seguro que deseas eliminar esta habitación?')) {
@@ -94,11 +94,11 @@ editarHabitacion(hab: Habitacion): void {
   ngOnInit(): void {
     this.habitacion$ = this.serv.getAllHabitaciones();
     this.categorias$ = this.catServ.getAllCategorias();
-    
+
   }
 
   compararCategoria = (c1: CategoriaHabitacion, c2: CategoriaHabitacion): boolean => {
-  return c1 && c2 ? c1.id === c2.id : c1 === c2;
-}
+    return c1 && c2 ? c1.id === c2.id : c1 === c2;
+  }
 
 }
