3ffc1b0a472b8dcd3b34fe3a40864f5cd32dfbfb
Daniela <danielasf2002@outlook.es>
2025-07-19 23:44:11 -0500

    servicios get listo pero problemas con la recarga de pagina

--- src/app/features/publico/servicios/servicios.ts
+++ src/app/features/publico/servicios/servicios.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Servicio } from '../../auth/models/servicio';
+import { ServicioService } from '../../admin/services/servicio.service';
 
 @Component({
   selector: 'app-servicios',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './servicios.html',
   styleUrl: './servicios.scss'
 })
-export class Servicios {
+export class Servicios implements OnInit {
+  servicios: Servicio[] = [];
 
+  constructor(private servicioService: ServicioService) {}
+
+  ngOnInit(): void {
+    this.servicioService.listar().subscribe(data => {
+      this.servicios = data;
+      console.log('Servicios cargados:', data);
+    });
+  }
 }
