ea2c40a70c0a89bc50210ab8b4ebaaa599acc03a
Daniela <danielasf2002@outlook.es>
2025-07-20 00:42:18 -0500

    se agrego campo de descripcion en reseñas y ya se visualiza con los get

--- src/app/features/publico/nosotros/nosotros.ts
+++ src/app/features/publico/nosotros/nosotros.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ResenaService } from '../../admin/services/resena.service';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { Resena } from '../../auth/models/resena';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nosotros',
-  imports: [],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './nosotros.html',
   styleUrl: './nosotros.scss'
 })
-export class Nosotros {
+export class Nosotros implements OnInit {
+  private servResena = inject(ResenaService);
+  rese$!: Observable<Resena[]>;
 
+  ngOnInit() {
+    this.rese$ = this.servResena.listar().pipe(
+      tap(data => console.log('Reseñas públicas:', data)),
+      catchError(error => {
+        console.error('Error cargando reseñas:', error);
+        return of([]);
+      })
+    );
+  }
 }
